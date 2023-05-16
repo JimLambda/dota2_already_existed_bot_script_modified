e6a0e01e1d08ea2aef5d29650e19da424bc3f6ce
SuperJim <526554176@qq.com>
2023-05-16 17:58:21 +0900

    Fixed selecting repeat hero.

--- hero_selection.lua
+++ hero_selection.lua
@@ -701,15 +701,15 @@ end
 function X.IsRepeatHero( sHero )
 
 	-- for id = 0, 20
-	for id = 0, 4
-	do
-		if ( IsTeamPlayer( id ) and GetSelectedHeroName( id ) == sHero )
-			or ( IsCMBannedHero( sHero ) )
-			or ( X.IsBanByChat( sHero ) )
-		then
-			return true
-		end
-	end
+	-- for id = 0, 4
+	-- do
+	-- 	if ( IsTeamPlayer( id ) and GetSelectedHeroName( id ) == sHero )
+	-- 		or ( IsCMBannedHero( sHero ) )
+	-- 		or ( X.IsBanByChat( sHero ) )
+	-- 	then
+	-- 		return true
+	-- 	end
+	-- end
 
 	return false
 
